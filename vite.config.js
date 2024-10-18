baeac3cb76c2c16cda02ead9ee172ebdce164edd
unknown <rqdavismail@gmail.com>
2024-10-18 16:21:25 -0700

    added base in sveltekit config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-  server: {
-    port: 5173, // No HTTPS configuration needed
-  },
+  base: '/cohere-integration/',  // Replace 'repository-name' with your actual repository name
+//   server: {
+//     port: 5173, // No HTTPS configuration needed
+//   },
 });
